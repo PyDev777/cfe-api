31a215f49ca3ea57f60a6d4d3395a7e7ac5a175e
dev <pydev@ukr.net>
2019-03-28 20:36:51 +0200

    Lessons 01-10

--- updates/models.py
+++ updates/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+def upload_update_image(instance, filename):
+    return "updates/{{user}}/{{filename}}".format(user=instance.user, filename=filename)
+
+
+class Update(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=upload_update_image, blank=True, null=True)
+
+    def __str__(self):
+        return self.content or ""
--- updates/views.py
+++ updates/views.py
@@ -1,3 +1,34 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views.generic import View
+from cfeapi.mixins import JsonResponseMixin
+from .models import Update
 
-# Create your views here.
+
+def json_example_view(request):
+    data = {
+        'count': 1000,
+        'content': 'Some new content',
+    }
+    json_data = json.dumps(data)
+    return HttpResponse(json_data, content_type='application/json')
+
+
+class JsonCBV(View):
+
+    def get(self, request, *args, **kwargs):
+        data = {
+            'count': 1000,
+            'content': 'Some new content',
+        }
+        return JsonResponse(data)
+
+
+class JsonCBV2(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'count': 1000,
+            'content': 'Some new content',
+        }
+        return self.render_to_json_response(data)
