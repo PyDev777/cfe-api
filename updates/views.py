d53a4750a080f690d09e4b27df29b11bf1e86d90
dev <pydev@ukr.net>
2019-03-28 23:41:35 +0200

    Lessons 11. Serialize Data

--- updates/views.py
+++ updates/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.generic import View
 from cfeapi.mixins import JsonResponseMixin
 from .models import Update
+from django.core.serializers import serialize
 
 
 def json_example_view(request):
@@ -32,3 +33,25 @@ class JsonCBV2(JsonResponseMixin, View):
             'content': 'Some new content',
         }
         return self.render_to_json_response(data)
+
+
+class SerializedDetailView(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        obj = Update.objects.get(id=1)
+        data = serialize("json", [obj, ], fields=('user', 'content'))
+        json_data = data
+        return HttpResponse(json_data, content_type='application/json')
+
+
+class SerializedListView(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        data = serialize("json", qs, fields=('user', 'content'))
+        print(data)
+        # data = {
+        #     'user': obj.user.username,
+        #     'content': obj.content,
+        # }
+        # json_data = json.dumps(data)
+        json_data = data
+        return HttpResponse(json_data, content_type='application/json')
