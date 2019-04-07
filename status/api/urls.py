4d06ba16d194a659a8b40b081bb49dc84ecdccca
dev <pydev@ukr.net>
2019-04-07 23:31:13 +0300

    Lessons 35. Update & Delete API Views

--- status/api/urls.py
+++ status/api/urls.py
@@ -4,15 +4,15 @@ status-api URL Configuration
 
 from django.conf.urls import url
 # from .views import StatusListSearchAPIView
-from .views import StatusAPIView, StatusCreateAPIView, StatusDetailAPIView
+from .views import StatusAPIView, StatusCreateAPIView, StatusDetailAPIView, StatusUpdateAPIView, StatusDeleteAPIView
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
     # url(r'^$', StatusListSearchAPIView.as_view()),
     url(r'^create/$', StatusCreateAPIView.as_view()),
-    url(r'^(?P<pk>.*)/$', StatusDetailAPIView.as_view()),
-    # url(r'^(?P<id>.*)/update/$', StatusUpdateAPIView.as_view()),
-    # url(r'^(?P<id>.*)/delete/$', StatusDeleteAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/$', StatusDetailAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/update/$', StatusUpdateAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/delete/$', StatusDeleteAPIView.as_view()),
 ]
 
 
