7ac1a0b378aa3177dc57e59c0ee36e2bec4a23e4
dev <pydev@ukr.net>
2019-04-07 23:17:47 +0300

    Lessons 34. Detail API View

--- status/api/urls.py
+++ status/api/urls.py
@@ -4,13 +4,13 @@ status-api URL Configuration
 
 from django.conf.urls import url
 # from .views import StatusListSearchAPIView
-from .views import StatusAPIView, StatusCreateAPIView
+from .views import StatusAPIView, StatusCreateAPIView, StatusDetailAPIView
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
     # url(r'^$', StatusListSearchAPIView.as_view()),
     url(r'^create/$', StatusCreateAPIView.as_view()),
-    # url(r'^(?P<id>.*)/$', StatusDetailAPIView.as_view()),
+    url(r'^(?P<pk>.*)/$', StatusDetailAPIView.as_view()),
     # url(r'^(?P<id>.*)/update/$', StatusUpdateAPIView.as_view()),
     # url(r'^(?P<id>.*)/delete/$', StatusDeleteAPIView.as_view()),
 ]
