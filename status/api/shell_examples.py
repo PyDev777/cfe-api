75a36ec7d9f7671a128344ef6f455d34829e83a8
dev <pydev@ukr.net>
2019-04-07 18:53:07 +0300

    Lessons 29. Create & Update through Serializers

--- status/api/shell_examples.py
+++ status/api/shell_examples.py
@@ -16,21 +16,11 @@ from rest_framework.parsers import JSONParser
 # ------------------------------------------
 
 obj = Status.objects.first()
-# print(obj)
-
 obj_serializer = StatusSerializer(obj)
-# print(obj_serializer)
-# print(obj_serializer.data)
-
 obj_json_data = JSONRenderer().render(obj_serializer.data)
-# print(obj_json_data)
-# print(json.loads(obj_json_data))
 
 obj_stream = io.BytesIO(obj_json_data)
-# print(obj_stream)
-
 obj_data = JSONParser().parse(obj_stream)
-# print(obj_data)
 
 
 # ------------------------------------------
@@ -38,98 +28,55 @@ obj_data = JSONParser().parse(obj_stream)
 # ------------------------------------------
 
 qs = Status.objects.all()
-# print(qs)
-
 qs_serializer = StatusSerializer(qs, many=True)
-# print(qs_serializer)
-# print(qs_serializer.data)
-
 qs_json_data = JSONRenderer().render(qs_serializer.data)
-# print(qs_json_data)
-# print(json.loads(qs_json_data))
 
 qs_stream = io.BytesIO(qs_json_data)
-# print(qs_stream)
-
 qs_data = JSONParser().parse(qs_stream)
-# print(qs_data)
 
 
-'''
-/home/dev/Proj/restapi2/bin/python3.6 /home/dev/Downloads/pycharm-2018.3.4/helpers/pydev/pydevconsole.py --mode=client --port=44073
 
-import sys; print('Python %s on %s' % (sys.version, sys.platform))
-import django; print('Django %s' % django.get_version())
-sys.path.extend(['/home/dev/Proj/restapi2/src/cfeapi', '/home/dev/Downloads/pycharm-2018.3.4/helpers/pycharm', '/home/dev/Downloads/pycharm-2018.3.4/helpers/pydev'])
-if 'setup' in dir(django): django.setup()
-import django_manage_shell; django_manage_shell.run("/home/dev/Proj/restapi2/src/cfeapi")
-PyDev console: starting.
 
-Python 3.6.8 (default, Dec 24 2018, 19:24:27) 
-[GCC 5.4.0 20160609] on linux
-Django 1.11.20
+# ------------------------------------------
+# Create object
+# ------------------------------------------
 
->>> import json
-... from status.models import Status
-... from status.api.serializers import StatusSerializer
-... from rest_framework.renderers import JSONRenderer
-... import io
-... from rest_framework.parsers import JSONParser
+data = {'user': 1, 'content': 'Comment is Created!'}
+create_obj_serializer = StatusSerializer(data=data)
 
->>> obj = Status.objects.first()
->>> obj
-<Status: Status Content 1...>
+create_obj_serializer.is_valid()
+create_obj = create_obj_serializer.save()
 
->>> obj_serializer = StatusSerializer(obj)
->>> obj_serializer
-StatusSerializer(<Status: Status Content 1...>):
-    user = PrimaryKeyRelatedField(queryset=User.objects.all())
-    content = CharField(allow_blank=True, allow_null=True, required=False, style={'base_template': 'textarea.html'})
-    image = ImageField(allow_null=True, max_length=100, required=False)
->>> obj_serializer.data
-{'user': 1, 'content': 'Status Content 1', 'image': None}
+# if serializer.is_valid():
+#     serializer.save()
 
->>> obj_json_data = JSONRenderer().render(obj_serializer.data)
->>> obj_json_data
-b'{"user":1,"content":"Status Content 1","image":null}'
->>> json.loads(obj_json_data)
-{'user': 1, 'content': 'Status Content 1', 'image': None}
 
->>> obj_stream = io.BytesIO(obj_json_data)
->>> obj_stream
-<_io.BytesIO object at 0x7f9fe9236830>
+# ------------------------------------------
+# Update object
+# ------------------------------------------
 
->>> obj_data = JSONParser().parse(obj_stream)
->>> obj_data
-{'user': 1, 'content': 'Status Content 1', 'image': None}
+obj = Status.objects.first()
+data = {'user': 1, 'content': 'Comment is Updated!'}
+update_serializer = StatusSerializer(obj, data=data)
 
->>> 
+update_serializer.is_valid()
+update_serializer.save()
 
->>> qs = Status.objects.all()
->>> qs
-<StatusQuerySet [<Status: Status Content 1...>, <Status: 13...>]>
+# update_serializer.errors
 
->>> qs_serializer = StatusSerializer(qs, many=True)
->>> qs_serializer
-StatusSerializer(<StatusQuerySet [<Status: Status Content 1...>, <Status: 13...>]>, many=True):
-    user = PrimaryKeyRelatedField(queryset=User.objects.all())
-    content = CharField(allow_blank=True, allow_null=True, required=False, style={'base_template': 'textarea.html'})
-    image = ImageField(allow_null=True, max_length=100, required=False)
->>> qs_serializer.data
-[OrderedDict([('user', 1), ('content', 'Status Content 1'), ('image', None)]), OrderedDict([('user', 1), ('content', '13'), ('image', None)])]
 
->>> qs_json_data = JSONRenderer().render(qs_serializer.data)
->>> qs_json_data
-b'[{"user":1,"content":"Status Content 1","image":null},{"user":1,"content":"13","image":null}]'
->>> json.loads(qs_json_data)
-[{'user': 1, 'content': 'Status Content 1', 'image': None}, {'user': 1, 'content': '13', 'image': None}]
+# ------------------------------------------
+# Delete object
+# ------------------------------------------
 
->>> qs_stream = io.BytesIO(qs_json_data)
->>> qs_stream
-<_io.BytesIO object at 0x7f9fe92368e0>
+data = {'user': 1, 'content': 'Delete me'}
+create_obj_serializer = StatusSerializer(data=data)
 
->>> qs_data = JSONParser().parse(qs_stream)
->>> qs_data
-[{'user': 1, 'content': 'Status Content 1', 'image': None}, {'user': 1, 'content': '13', 'image': None}]
+create_obj_serializer.is_valid()
+create_obj = create_obj_serializer.save()
+print(create_obj)
 
-'''
+obj = Status.objects.last()
+get_data_serializer = StatusSerializer(obj)
+print(get_data_serializer.data)
+print(obj.delete())
