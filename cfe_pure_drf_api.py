eeb0a5c8bcaa2cea29d05d3864d0807025200718
dev <pydev@ukr.net>
2019-04-09 01:06:05 +0300

    Lessons 44. Implement JWT Authentication

--- cfe_pure_drf_api.py
+++ cfe_pure_drf_api.py
@@ -2,25 +2,67 @@ import json
 import requests
 import os
 
-BASE_URL = 'http://192.168.1.5:8000/'
-ENDPOINT = 'api/status/'
+AUTH_ENDPOINT = 'http://192.168.1.5:8000/api/auth/jwt/'
+REFRESH_ENDPOINT = AUTH_ENDPOINT + 'refresh/'
 
 image_path = os.path.join('drf-logo.jpg')
 
-get_endpoint = BASE_URL + ENDPOINT + str(25)
-post_data = json.dumps({'content': 'Some random content'})
+data = {
+    'username': 'dev',
+    'password': '1212qwqw'
+}
 
-r = requests.get(get_endpoint)
-print('r.text:', r.text)
+headers = {'content-type': 'application/json'}
 
-r2 = requests.get(BASE_URL + ENDPOINT)
-print('r2.status_code:', r2.status_code)
+r = requests.post(AUTH_ENDPOINT, data=json.dumps(data), headers=headers)
+print('status_code = ', r.status_code)
+print("headers['content-type'] = ", r.headers.get('content-type', None))
+if r.status_code == requests.codes.ok:
+    print('Status code is:', r.status_code, '(OK)')
+    # print('r.json():', r.json())
+    token = r.json().get('token', None)
+    print('TOKEN:', token)
 
-post_headers = {'content-type': 'application/json'}
+    refresh_data = {'token': token}
+    r2 = requests.post(REFRESH_ENDPOINT, data=json.dumps(refresh_data), headers=headers)
+    print('status_code (refresh) = ', r2.status_code)
+    print("headers['content-type'] (refresh) = ", r2.headers.get('content-type', None))
+    if r2.status_code == requests.codes.ok:
+        print('Status code is (refresh):', r2.status_code, '(OK)')
+        # print('r.json():', r.json())
+        token2 = r2.json().get('token', None)
+        print('TOKEN (refresh):', token2)
+    else:
+        print('Status code is (refresh):', r2.status_code, '(NON-200!)')
+        print('r2.text (refresh) = ', r2.text)
+else:
+    print('Status code is:', r.status_code, '(NON-200!)')
+    print('r.text = ', r.text)
 
-post_response = requests.post(BASE_URL + ENDPOINT, data=post_data, headers=post_headers)
 
-print('post_response.text:', post_response.text)
+
+
+
+
+# -------------------------------------------------------------------------------------------
+
+# BASE_URL = 'http://192.168.1.5:8000/'
+# ENDPOINT = 'api/status/'
+
+# get_endpoint = BASE_URL + ENDPOINT + str(25)
+# post_data = json.dumps({'content': 'Some random content'})
+#
+# r = requests.get(get_endpoint)
+# print('r.text:', r.text)
+#
+# r2 = requests.get(BASE_URL + ENDPOINT)
+# print('r2.status_code:', r2.status_code)
+#
+# post_headers = {'content-type': 'application/json'}
+#
+# post_response = requests.post(BASE_URL + ENDPOINT, data=post_data, headers=post_headers)
+#
+# print('post_response.text:', post_response.text)
 
 
 # def do_img(method='get', data={}, is_json=True, img_path=None):
