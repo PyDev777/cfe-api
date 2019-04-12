532d658d0fec43fdac2ad0c47b3e398ca8dc8f6c
dev <pydev@ukr.net>
2019-04-12 15:14:59 +0300

    Lessons 61. Automated Testing

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(username='dev', email='pydev@ukr.net')
+        user.set_password('1212qwqw')
+        user.save()
+
+    def test_created_user(self):
+        qs = User.objects.filter(username='dev')
+        self.assertEqual(qs.count(), 1)
--- status/tests.py
+++ status/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from status.models import Status
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+class StatusTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create(username='dev', email='pydev@ukr.net')
+        user.set_password('1212qwqw')
+        user.save()
+
+    def test_created_status(self):
+        user = User.objects.get(username='dev')
+        obj = Status.objects.create(user=user, content='Test content 1')
+        self.assertEqual(obj.id, 1)
+
+        qs = Status.objects.all()
+        self.assertEqual(qs.count(), 1)
